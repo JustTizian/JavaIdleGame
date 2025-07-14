6f97b4f0045a3b547635386df63eefdf2117129a
tihi <77891364+JustTizian@users.noreply.github.com>
2025-07-14 13:45:53 +0200

    fix typo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("This is  anew feature!");
+        System.out.println("This is a new feature!");
     }
 }
